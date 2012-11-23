45ca75d633d9412b3a164a4dc7bf760ed5651e47
M.K.Ramkumar <ram.psg.cse@gmail.com>
2012-11-23 12:57:18 +0530

    ram | initial rails checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picviewer::Application.config.secret_token = '7f8a73a141631be9d89a471036adfbee2feb76d1d4fab346729effcf0797c6157ac4f410f7b93fc9acc9589b37fe01ba4c697b7f2736253d88753c1c1d0a748d'
